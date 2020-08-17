74753b09158f71da024f23d89731daef5019a539
Carlo <carlo.dri@gmail.com>
2020-08-17 16:01:49 +0200

    try to improve metapackage recipe
    see #53

--- recipe/build_base.sh
+++ recipe/build_base.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
 set -euo pipefail
 
-${PYTHON} -m pip install . --no-deps -vv
+PIP_NO_BUILD_ISOLATION=False \
+    PIP_NO_DEPENDENCIES=True \
+    PIP_IGNORE_INSTALLED=True \
+    PIP_NO_INDEX=True \
+    PYTHONDONTWRITEBYTECODE=True \
+    ${PYTHON} -m pip install . --no-deps -vv
